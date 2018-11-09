8e2792bfef1d84b70d6e91ba16f6587c43410ade
myyw1027 <myyw1027@sina.com>
2018-11-09 15:13:52 +0800

    333
    432432

--- mylibrary/src/main/java/com/example/mylibrary/FcUtils.java
+++ mylibrary/src/main/java/com/example/mylibrary/FcUtils.java
@@ -1,8 +1,15 @@
 package com.example.mylibrary;
 
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 public class FcUtils {
-    public void dfd(){
-        System.out.print("433");
+    public void dfd(Context context, String url, ImageView imageView){
+        Glide.with(context)
+                .load(url)
+                .into(imageView);
 
     }
 }
